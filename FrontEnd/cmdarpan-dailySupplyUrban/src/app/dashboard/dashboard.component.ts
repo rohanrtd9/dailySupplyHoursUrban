8b72dd3afe9795f5abcc4295e9c9c8a7f68f4350
rohanrtd9 <rohanrtd9@gmail.com>
2023-07-17 12:49:14 +0530

    New changes

--- FrontEnd/cmdarpan-dailySupplyUrban/src/app/dashboard/dashboard.component.ts
+++ FrontEnd/cmdarpan-dailySupplyUrban/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as XLSX from 'xlsx';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -53,7 +56,25 @@ export class DashboardComponent {
     const percentage = (given / scheduled) * 100;
     return percentage.toFixed(2) + '%';
   }
+  exportDown()
+  {
+    const doc = new jsPDF()
+
+
+    // Set the position and size of the header image
+    const headerX = 10;
+    const headerY = 10;
+    const headerWidth = 150;
+    const headerHeight = 30;
   
+    // Add the image to the PDF
+    doc.addImage("assets/logo.png", 'PNG', headerX, headerY, headerWidth, headerHeight);
+  
+    // Parse HTML table
+    autoTable(doc, { html: '#downTable', startY: headerY + headerHeight + 10, styles: { lineWidth: .5 } });
+  
+    doc.save('table.pdf')
+  }
   getProgressBarColor(given: number, scheduled: number): string {
     const percentage = (given / scheduled) * 100;
     if (percentage >= 70) {
